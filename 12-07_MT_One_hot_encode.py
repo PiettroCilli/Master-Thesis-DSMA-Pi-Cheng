a33caf5d5a652aa7f39e1d75fad20d5895000dba
PiettroCilli <133371988+PiettroCilli@users.noreply.github.com>
2023-08-24 02:01:17 +0200

    Add files via upload

--- 12-07_MT_One_hot_encode.py
+++ 12-07_MT_One_hot_encode.py
@@ -1,15 +1,22 @@
 #Loading libraries
 import pandas as pd
+import gc         
 
 #Loading data
-data = pd.read_csv("C:/Users/Administrator/Documents/Msc Data Science & Marketing Analytics/Master thesis/Data sets/Merged Instacart/data_small_transformed.csv")
+data = pd.read_csv("./data_big_transformed.csv")
 
 #Setting display options
-pd.set_option("display.max_rows", 20)
-pd.set_option("display.max_columns", 20)
+pd.set_option("display.max_rows", None)
+pd.set_option("display.max_columns", None)
+
+#Take a subset of full merged data
+data1 = data.loc[data.user_id.isin(data.user_id.drop_duplicates().sample(frac=0.5, random_state=16))] 
+data = data1
+del data1
+gc.collect()
 
 #Look at general information
-data.nunique()
+data.nunique(axis=0)
 data.info()
 data.describe(include = 'all')
 
@@ -22,7 +29,7 @@ pivot_data = data.pivot(index='user_id', columns='product_name', values='sticky'
 
 # Fill NaN values with 0 (representing that the user has not bought the product)
 pivot_data = pivot_data.fillna(0)
-
+ 
 # Reset the index to make user_id a column again
 pivot_data = pivot_data.reset_index()
 
@@ -31,10 +38,12 @@ user_data = data.groupby('user_id').first().reset_index()[['user_id', 'avg_days_
 
 # Merge the user_data with the pivot_data
 final_data = pd.merge(user_data, pivot_data, on='user_id')
+del data, pivot_data, user_data
 
 #Opslaan van CSV file
-final_data.to_csv("data_small_one-hot.csv", index = False)
+final_data.to_csv("data_big_one-hot.csv", index = False)
+data = final_data
+del final_data
 
-
-
- 
+data.iloc[1:25, [0, 1, 7, 8, 9, 13]] 
+data.iloc[1:25, 3000:3020] 
